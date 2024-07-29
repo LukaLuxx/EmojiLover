2bbf19e076170a3ebb46c4f6894a89ef6417e1bf
LukaLuxx <luka.jambros@gmail.com>
2024-07-29 10:53:42 +0200

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Luka Jambrošić on 29.07.2024..
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
